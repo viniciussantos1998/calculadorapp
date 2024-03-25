56a9367d1b124d3fa4cd827be39a815965efafef
viniciussantos1998 <142357302+viniciussantos1998@users.noreply.github.com>
2024-03-25 20:55:48 -0300

    Add files via upload

---
+++ sdasd.py
@@ -0,0 +1,17 @@
+class aluno:
+    pass
+    def __init__(self, nome, idade, cpf):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+
+
+aluno1 = aluno("Matheus", 15, 12955623407)
+aluno2  = aluno("Matheus", 15, 12955623407)
+aluno3= aluno("Matheus", 15, 12955623407)
+aluno4 = aluno("Matheus", 15, 12955623407)
+aluno5 = aluno("Matheus", 15, 12955623407)
+aluno= aluno("Matheus", 15, 12955623407)
+
+
+print(aluno1.nome, aluno1.idade, aluno1.cpf)
