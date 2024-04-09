481572d988a22b64255d79c89c78f7083f99a568
viniciussantos1998 <142357302+viniciussantos1998@users.noreply.github.com>
2024-04-08 21:13:23 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '.components\Button';
+import Row from './components/Row';
+import calculator,{initialState} from './util/calculator';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>luana²¢¬</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = initialState;
+  HandleTap = (type, value) =>{
+  this.setState((state) => calculator(type, value, state));
+  };
+  reder(){
+    return(
+      <View style = {style.container}>
+        <SafeAreaView>
+          <Text style={style.value}>
+            {parseFloat(this.state.currentValue).toLocaleString()}
+          </Text>
+        </SafeAreaView>
+        
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
