2a9c46a262fb1497f76c264d73f31c705394ba96
viniciussantos1998 <142357302+viniciussantos1998@users.noreply.github.com>
2024-04-15 20:28:04 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,34 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from '.components\Button';
+import { SafeAreaView,StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
 import Row from './components/Row';
-import calculator,{initialState} from './util/calculator';
-import { Component } from 'react';
+import calculator, {initialState} from './util/calculator';
+import React, { Component } from 'react';
 
 export default class App extends Component {
   state = initialState;
-  HandleTap = (type, value) =>{
-  this.setState((state) => calculator(type, value, state));
+
+  HandleTap = (type, value) => {
+    this.setState((state) => calculator(type, value, state));
   };
-  reder(){
-    return(
-      <View style = {style.container}>
-        <SafeAreaView>
-          <Text style={style.value}>
-            {parseFloat(this.state.currentValue).toLocaleString()}
-          </Text>
-        </SafeAreaView>
-        
+
+  render(){
+    return (
+      <View style={styles.container}>
+       <SafeAreaView>
+        <Text style={styles.value}>
+          {parseFloat(this.state.currentValue).toLocaleString()}
+        </Text>
+
+        { /*component Row - primeira linha*/}
+        <Row>
+          <Button
+            text="C"
+            theme="secondary"
+            onPress={()=> this.HandleTap("clear")}
+          />
+          <Button
+            text="+/-"
+            theme="secondary"
+            onPress={()=> this.HandleTap("posneg")}
+          />
+          <Button
+            text="%"
+            theme="secondary"
+            onPress={()=> this.HandleTap("percentage")}
+          />
+          <Button
+            text="/"
+            theme="accent"
+            onPress={()=> this.HandleTap("operator", "/")}
+          />
+        </Row>
+        { /*segunda linha*/}
+        <Row>
+          <Button text="7" onPress={() => this.HandleTap("number", 7)}/>
+          <Button text="8" onPress={() => this.HandleTap("number", 8)}/>
+          <Button text="9" onPress={() => this.HandleTap("number", 9)}/>
+          <Button text="X" theme="accent" onPress={() => this.HandleTap("operator", "*")}/>
+        </Row>
+        { /*terceira linha*/}
+        <Row>
+          <Button text="4" onPress={() => this.HandleTap("number", 4)}/>
+          <Button text="5" onPress={() => this.HandleTap("number", 5)}/>
+          <Button text="6" onPress={() => this.HandleTap("number", 6)}/>
+          <Button text="-" 
+           theme="accent"
+           onPress={() => this.HandleTap("operator", "-")}/>
+        </Row>
+        { /*quarta linha*/}
+        <Row>
+          <Button text="1" onPress={() => this.HandleTap("number", 1)}/>
+          <Button text="2" onPress={() => this.HandleTap("number", 2)}/>
+          <Button text="3" onPress={() => this.HandleTap("number", 3)}/>
+          <Button text="+" 
+           theme="accent"
+           onPress={() => this.HandleTap("operator", "+")}/>
+        </Row>
+        { /*quinta linha*/}
+        <Row>
+          <Button text="0" onPress={() => this.HandleTap("number", 0)}/>
+          <Button text="." onPress={() => this.HandleTap("number", ".")}/>
+          <Button text="="
+           theme="primary"
+           onPress={() => this.HandleTap("equal", "=")}/>
+        </Row>
+        </SafeAreaView> 
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#202020',
+    justifyContent: 'flex-end',
+  },
+  value:{
+    color:"#fff",
+    fontSize: 42,
+    textAlign: "right",
+    marginRight: 20,
+    marginBottom: 10,
   },
 });
